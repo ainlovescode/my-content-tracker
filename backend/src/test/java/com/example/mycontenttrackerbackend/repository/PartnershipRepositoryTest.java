07a9a52750732dcb6f8ecb73cb192ec7d610ff78
Ainul Mardhiyyah <ainulmardhiyyah@Ainuls-Personal-MacBook-Pro.local>
2023-05-01 14:59:26 +0800

    [Ain] 1 - chore: verify only partnerships with specific partner is retrieved

--- backend/src/test/java/com/example/mycontenttrackerbackend/repository/PartnershipRepositoryTest.java
+++ backend/src/test/java/com/example/mycontenttrackerbackend/repository/PartnershipRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -36,12 +38,16 @@ class PartnershipRepositoryTest {
 
     @Test
     public void shouldRetrievePartnershipPoGivenPartnerName() {
-        var partnership = PartnershipPo.builder()
+        var partnershipA = PartnershipPo.builder()
                 .partnerName("Partner A")
                 .partnershipFee(12345)
                 .build();
+        var partnershipB = PartnershipPo.builder()
+                .partnerName("Partner B")
+                .partnershipFee(12345)
+                .build();
 
-        partnershipRepository.save(partnership);
+        partnershipRepository.saveAll(List.of(partnershipA, partnershipB));
 
         var retrievedPartnerships = partnershipRepository.findByPartnerName("Partner A");
 
